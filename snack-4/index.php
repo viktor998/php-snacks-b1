a556df21bc5055f5421c3621e95402189dadda25
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-04-29 18:49:42 +0200

    Snack terminati

---
+++ snack-4/index.php
@@ -0,0 +1,84 @@
+<?php 
+
+$students = [
+
+    'Studente 1' =>[
+        
+        'nome' => 'Michaelo Viktor',
+        'cognome' => 'Casubolo',
+        'voti' => [
+            'matematica' => 8,
+            'informatica' => 7,
+            'fisica' => 8
+        ]
+        
+        
+    ],
+    'Studente 2' =>[
+        
+        'nome' => 'Mario',
+        'cognome' => 'Rossi',
+        'voti' => [
+            'matematica' => 7,
+            'informatica' => 7,
+            'fisica' =>6
+        ]
+        
+    ],
+    'Studente 3' =>[
+        
+        'nome' => 'John',
+        'cognome' => 'Doe',
+        'voti' => [
+            'matematica' => 8,
+            'informatica' => 7,
+            'fisica' => 7
+        ]
+        
+        
+    ],
+];
+
+
+
+$key = array_keys($students);
+$somma = 0;
+
+    for ($i=0; $i < count($students); $i++) { 
+        
+        ?>
+    
+        <div style='padding-top: 15px'>
+            <?= $key[$i] . '<br>' ?>
+            
+            <span> Nome: <?= $students[$key[$i]]['nome'] . '<br>'?></span>
+            <span> Cognome: <?= $students[$key[$i]]['cognome'] . '<br>'?></span>
+          
+            <?php 
+            for ($j=0; $j < count($students[$key[$i]]['voti']); $j++) {  ?>
+
+                <?php $key_voti = array_keys($students[$key[$i]]['voti']) ?>
+                
+                <span><?= $key_voti[$j] . ' : ' . $students[$key[$i]]['voti'][$key_voti[$j]] . '<br>'?>  </span>
+
+                <?php 
+                
+                    $somma += $students[$key[$i]]['voti'][$key_voti[$j]];
+                    
+                
+                ?>
+                <?php
+            }
+
+            $media = $somma / count($students[$key[$i]]['voti']);
+
+            
+            
+            ?>
+            <span> La media dei voti è:  <?= $media ?> </span>
+            <?php $somma = 0; ?>
+        </div>
+        <?php
+    }
+
+?>
